0057ddfca758e10e1ffa3cf801ee5ee3d75073f8
Simon Lammmes <ghostsgandi@gmail.com>
2023-04-15 20:44:27 +0200

    wip designer

--- src/app/shared/components/exercise-preview/exercise-preview.component.ts
+++ src/app/shared/components/exercise-preview/exercise-preview.component.ts
@@ -11,11 +11,18 @@ import { TrustHtmlPipe } from '../../pipes/trust-html.pipe';
 import { ExerciseComponent } from '../exercise/exercise.component';
 import { Exercise } from '../../models/exercise';
 import { Label } from '../../models/label';
+import { CopyTextButtonComponent } from '../copy-text-button/copy-text-button.component';
 
 @Component({
   selector: 'app-exercise-preview',
   standalone: true,
-  imports: [CommonModule, MatCardModule, TrustHtmlPipe, ExerciseComponent],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    TrustHtmlPipe,
+    ExerciseComponent,
+    CopyTextButtonComponent,
+  ],
   templateUrl: './exercise-preview.component.html',
   styleUrls: ['./exercise-preview.component.sass'],
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/shared/components/label-preview/label-preview.component.ts
+++ src/app/shared/components/label-preview/label-preview.component.ts
@@ -8,11 +8,12 @@ import {
 import { CommonModule } from '@angular/common';
 import { LocalAssetUrlPipe } from '../../pipes/local-asset-url.pipe';
 import { Label } from '../../models/label';
+import { CopyTextButtonComponent } from '../copy-text-button/copy-text-button.component';
 
 @Component({
   selector: 'app-label-preview',
   standalone: true,
-  imports: [CommonModule, LocalAssetUrlPipe],
+  imports: [CommonModule, LocalAssetUrlPipe, CopyTextButtonComponent],
   templateUrl: './label-preview.component.html',
   styleUrls: ['./label-preview.component.sass'],
   changeDetection: ChangeDetectionStrategy.OnPush,
