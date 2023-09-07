c8ed1e37f6f6f4d4ff84a77cd949896c55db408e
Simon Lammmes <ghostsgandi@gmail.com>
2023-09-07 20:56:05 +0200

    upgrade deps

--- src/app/shared/custom-elements/math.element.ts
+++ src/app/shared/custom-elements/math.element.ts
@@ -1,5 +1,6 @@
 import { css, html, LitElement } from 'lit';
 import { customElement } from 'lit/decorators.js';
+// @ts-ignore
 import katex from 'katex';
 import { unsafeHTML } from 'lit/directives/unsafe-html.js';
 import * as _ from 'lodash';
