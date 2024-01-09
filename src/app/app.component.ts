872fd1d243d755d21ec8c91fb9d0caa9c110d8ee
Simon Lammes <ghostsgandi@gmail.com>
2024-01-09 19:40:54 +0100

    add prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,16 @@ import { CommonModule } from "@angular/common";
 import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <h1 class="text-sm dark:text-xl">Welcome to {{ title }}!</h1>
 
     <router-outlet></router-outlet>
   `,
   styles: [],
 })
 export class AppComponent {
-  title = 'conceptwire-angular';
+  title = "conceptwire-angular";
 }
