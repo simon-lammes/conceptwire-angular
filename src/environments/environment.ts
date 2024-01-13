6d3e37fc762dbdd7369fd5b9a67da7102b882c02
Simon Lammes <ghostsgandi@gmail.com>
2024-01-13 21:00:09 +0100

    wip

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  supabaseUrl: "https://detstpnvrddskwwhopek.supabase.co",
+  supabaseKey:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRldHN0cG52cmRkc2t3d2hvcGVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzMxOTM3MzYsImV4cCI6MTk4ODc2OTczNn0.jYqSnz-m6TAJKr5CeEbVJ0vEWgy_08kmTL20zuzgOCA",
+};
